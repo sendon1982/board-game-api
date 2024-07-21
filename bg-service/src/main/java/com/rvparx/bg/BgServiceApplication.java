098aa931cd0de28d0318ebacdcb7a90977377432
ming.jiang <sendon1982@gmail.com>
2024-07-21 22:20:43 +1000

    add rest template config

---
+++ bg-service/src/main/java/com/rvparx/bg/BgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rvparx.bg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgServiceApplication.class, args);
+	}
+
+}
---
+++ bg-service/src/test/java/com/rvparx/bg/BgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvparx.bg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
